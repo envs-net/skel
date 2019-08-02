ff470edb62eba23bcc9e3fe0244883ae39d6b512
cremesk <ennik@envs.net>
2019-08-03 00:17:33 +0200

    change fzf path

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/opt/fzf/bin"
+if [[ ! "$PATH" == */opt/services/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/opt/services/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/opt/services/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/opt/fzf/shell/key-bindings.bash"
+source "/opt/services/fzf/shell/key-bindings.bash"
