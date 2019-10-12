b77bf549951f15a7ff41a3dd41a888e9ef50f558
creme <creme@envs.net>
2019-10-12 03:07:02 +0200

    remove dark/light switch from user pages.

--- public_html/your_index_template.php
+++ public_html/your_index_template.php
@@ -12,7 +12,7 @@ YOU CAN REMOVE THIS COMMENT!
 <?php $user = posix_getpwuid(fileowner(__FILE__))["name"]; ?>
 <!-- HTML HEADER -->
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
 		<title>~<?=$user?> on envs.net</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -22,14 +22,13 @@ YOU CAN REMOVE THIS COMMENT!
 		<meta name="keywords" content="<?=$user?>" />
 		<link rel="stylesheet" href="https://envs.net/css/css_style.css" />
 		<link rel="stylesheet" href="https://envs.net/css/fork-awesome.min.css" />
-		<script src="https://envs.net/js/darklight.js" ></script>
 	</head>
 <!-- HTML BODY -->
 	<body id="body">
 		<!-- min-width: needed if the sidebar is enable -->
 		<div style="clear:both; /*min-width: 750px;*/">
 
-			<div id="main" style="float: left;">
+			<div id="main">
 <pre>
 <blockquote>
 <?=$user?>
@@ -54,15 +53,7 @@ more text
 			</div>
 
 -->
-
-<!-- HTML FOOTER -->
-			<!-- Button DarkLight -->
-			<div class="button_darklight">
-				<button type="button" name="dark_light" onclick="toggleDarkLight()" title="Toggle dark/light mode">🌛</button>
-			</div>
 		</div>
-
-<script>(function() { setThemeFromCookie() })();</script>
 
 	</body>
 </html>
